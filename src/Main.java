c8c69136058b0a98fcf02dd728f5bb380977e692
Lucas Silveira de Moura <lucasmour4@gmail.com>
2016-11-21 00:36:30 -0200

    :+1: Validators up and runnin

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.*;
 import java.util.ArrayList;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] argv){
         //OracleConnector.setCredentials("n8937250", "n8937250");
         //convertElectionDatabase("conversion.txt");
@@ -9,6 +15,18 @@ public class Main {
 
         //testInsertion();
         //testFind();
+
+        launch(argv);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/findwizardview.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Sisgate - Sistema Gerenciador de Atividades Eleitorais");
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
     }
 
     /**
